c27b0d63223da231bba10ee77ce4b10b9f81b821
nguyenxuanvang <nguyenvang14722@gmail.com>
2024-10-19 00:04:58 +0700

    sub commit

--- src/index.js
+++ src/index.js
@@ -63,11 +63,3 @@ root.render(
     <RouterProvider router={router} />
   </Provider>
 );
-//test change
-//bonus test
-//change from remote
-//bonus test change
-//change from remote 1
-// test rebase
-//change from remote 1
-
